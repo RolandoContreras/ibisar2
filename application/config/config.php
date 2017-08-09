d3065f7b79d3f1f69c65af3048324bec0ebcab6c
software.contreras@gmail.com <software.contreras@gmail.com>
2017-08-08 19:30:44 -0500

    changes on ibisar

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/ibisar2/';
 
 /*
 |--------------------------------------------------------------------------
@@ -34,7 +34,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -232,7 +232,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'c7850f98Tc0150Z2191Y29abb3f9fbc9';
 
 /*
 |--------------------------------------------------------------------------
