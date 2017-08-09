2fd80f4e266e53ca9163bf8b8e78cdf9aa8f098c
software.contreras@gmail.com <software.contreras@gmail.com>
2017-08-08 22:21:32 -0500

    add home view

---
+++ application/views/nav.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div id="logo"> <a class="logo" href="<?php echo site_url().'home';?>">Intact</a>
+            </div>
+            <div class="navbar-header page-scroll"> 
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-menu"> 
+                    <span class="icon-bar"></span> 
+                        <span class="icon-bar"></span> 
+                            <span class="icon-bar"></span> 
+                </button>
+            </div>
+            <div id="main-menu" class="collapse navbar-collapse  navbar-right">
+                <ul id="menu-main-menu" class="nav navbar-nav">
+                    <li id="menu-item-2377" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-2 current_page_item current-menu-ancestor current-menu-parent current_page_parent current_page_ancestor menu-item-has-children menu-item-2377 dropdown active">
+                        <a title="Home" href="<?php echo site_url().'home';?>">Home</a>
+                    </li>
+                    <li id="menu-item-2379" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2379 one-page-link">
+                        <a title="Services" href="http://keydesign-themes.com/intact/business/home/high-quality-services/">Services</a>
+                    </li>
+                    <li id="menu-item-2381" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2381 one-page-link">
+                        <a title="Team" href="http://keydesign-themes.com/intact/business/home/the-best-team-available/">Team</a>
+                    </li>
+                    <li id="menu-item-2383" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2383 one-page-link">
+                        <a title="Testimonials" href="http://keydesign-themes.com/intact/business/home/what-they-say/">Testimonials</a>
+                    </li>
+                    <li id="menu-item-2384" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2384 one-page-link">
+                        <a title="Contact" href="http://keydesign-themes.com/intact/business/home/get-in-touch/">Contact</a>
+                    </li>
+                    <li id="menu-item-2623" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-2623 dropdown">
+                        <a title="Works" href="#">Login</a>
+                    </li>
+                </ul> 
+            </div>
+        </div>
+    </nav>
